0c7de34cf8ee8173164681bd65b323839cfcfd23
zzzang12 <wadevs12@gmail.com>
2025-12-01 16:13:22 +0900

    ch.5 페이지 레이아웃

--- 이장원/browser.py
+++ 이장원/browser.py
@@ -1,11 +1,18 @@
 import sys
 import tkinter
 
-from layout import Layout, WIDTH, HEIGHT, HSTEP, VSTEP
+from block_layout import WIDTH, HEIGHT, HSTEP, VSTEP
+from document_layout import DocumentLayout
 from url import URL
-from parser import HTMLParser, print_tree
+from html_parser import HTMLParser, print_tree
 
 SCROLL_STEP = 100
+
+def paint_tree(layout_object, display_list):
+    display_list.extend(layout_object.paint())
+
+    for child in layout_object.children:
+        paint_tree(child, display_list)
 
 class Browser:
     def __init__(self):
@@ -22,19 +29,23 @@ class Browser:
 
     def draw(self):
         self.canvas.delete("all")
-        for x, y, c, f in self.display_list:
-            if y > self.scroll + HEIGHT: continue
-            if y + VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c, anchor="nw", font=f)
+        for cmd in self.display_list:
+            if cmd.top > self.scroll + HEIGHT: continue
+            if cmd.bottom < self.scroll: continue
+            cmd.execute(self.scroll, self.canvas)
 
     def load(self, url):
         body = url.request()
         self.nodes = HTMLParser(body).parse()
-        self.display_list = Layout(self.nodes).display_list
+        self.document = DocumentLayout(self.nodes)
+        self.document.layout()
+        self.display_list = []
+        paint_tree(self.document, self.display_list)
         self.draw()
 
     def scrolldown(self, e):
-        self.scroll += SCROLL_STEP
+        max_y = max(self.document.height + 2 * VSTEP - HEIGHT, 0)
+        self.scroll = min(self.scroll + SCROLL_STEP, max_y)
         self.draw()
 
 if __name__ == "__main__":
---
+++ 이장원/document_layout.py
@@ -0,0 +1,22 @@
+from block_layout import BlockLayout
+
+from block_layout import WIDTH, HEIGHT, HSTEP, VSTEP
+
+class DocumentLayout:
+    def __init__(self, node):
+        self.node = node
+        self.parent = None
+        self.children = []
+
+    def layout(self):
+        child = BlockLayout(self.node, self, None)
+        self.children.append(child)
+
+        self.width = WIDTH - 2 * HSTEP
+        self.x = HSTEP
+        self.y = VSTEP
+        child.layout()
+        self.height = child.height
+
+    def paint(self):
+        return []
